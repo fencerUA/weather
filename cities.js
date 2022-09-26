c057188d3ecb3fbf9ce99d7a6dc95070157d798f
VolodymB <volodymb.com>
2022-09-26 13:50:44 +0300

    citi array

--- cities.js
+++ cities.js
@@ -1,3 +1,22 @@
-<script src="sourse/city.list.json"></script>
-let data = JSON.parse(data);
-console.log(data);
+// let cities = [
+//     Europe: {[
+//        "Berlin","Paris","London","Madrid", "Vienna", "Brussels","Moscow",""
+//     ]},
+    
+// ];
+
+let regions = [
+    {name:"Europe", cities: ["Berlin","Paris","London","Madrid", "Vienna", "Brussels","Moscow"]},
+    {name:"America", cities: ["Washington","New York","Chicago"]},
+    {name:"Asia", cities: ["Kabul","Dhaka","Peking"]},
+    {name:"Africa", cities: ["Algiers","Luanda","Cairo"]},
+    {name:"Oceanic", cities: ["Canberra","Wellington"]}
+];
+
+
+
+let regList = regions.reduce((accumulator, region) => `${accumulator} ${region.name}, `, 'Regions: ');
+console.log(regList);
+
+// console.log(regions[id = 1].cities);
+
