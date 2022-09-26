108324f6f009fe22688a7bf41a3674c5e9a913ee
VolodymB <volodymb.com>
2022-09-26 15:24:56 +0300

    new

--- cities.js
+++ cities.js
@@ -14,13 +14,13 @@ let regions = [
 ];
 
 for(let i=0;i <regions.concat.length;i++){
-    // let regionName = regions[i].name;
+    let regionName = regions[i].name;
     let cities = regions[i].cities;
+    console.log(regionName);
     for(let y = 0;y <cities.length;y++){
         console.log(cities[y]);
     }
-    // console.log(regionName);
-    // console.log(cities.length);
+    
 }
 
 
